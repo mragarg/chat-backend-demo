26b1d695315bea2f78e81d8b4f223c2cdba790ed
Ashish Garg <mragarg@gmail.com>
2019-05-03 13:18:46 -0400

    Fix get and post link

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}));
+
+// This is my 'database'
+const db = [
+    'Welcome to the Chat App!'
+];
+
+// When GET request comes in,
+// send bac all the messages.
+app.get('/api', (req, res) => {
+    res.json(db);
+});
+
+// When POST request comes in,
+// add message to array of messages. 
+app.post('/api', (req, res) => {
+    // What to do
+    console.log(req.body.message);
+    db.push(req.body.message);
+    res.json({
+        'message': req.body.message
+    })
+})
 
 app.listen(31337, () => {
     console.log(`You're running!`);
